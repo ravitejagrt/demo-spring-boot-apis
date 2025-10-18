a2ea037bf809c74cf05bc6a1c3807c877a632227
ravitejagrt <raviteja.gajarla@gmail.com>
2025-10-18 16:02:31 -0500

    initial

---
+++ src/main/java/com/example/demo/DemoSpringBootApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootApisApplication.class, args);
+	}
+
+}
