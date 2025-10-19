03eb20d8a0eeada37dce5bd02467516ad453ad4d
ravitejagrt <raviteja.gajarla@gmail.com>
2025-10-19 18:19:54 -0500

    added github url in crossorigin

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {
+		"http://localhost:3000",
+		"https://ravitejagrt.github.io/my-first-react-app/"
+})
 public class HelloController {
 
     @GetMapping("/helloWorld")
