b432c30c6cd84a754a188d86e18e0854e70d848a
ravitejagrt <raviteja.gajarla@gmail.com>
2025-10-19 21:53:24 -0500

    added requesturi log

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @CrossOrigin(origins = {
 		"http://localhost:3000",
@@ -12,7 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/helloWorld")
-    public String hello() {
-        return "Hello World123";
+    public String hello(HttpServletRequest request) {
+    	String response = "Hello World!";
+    	String requestURI = request.getRequestURI();
+		
+    	System.out.println("Enpoint: " + requestURI + ". Response Text: " + response);
+        return response;
     }
 }
